bef05bcd7313d5252a1b4e8dc819781d26c1d331
PundrikThakkar <pundrikpundrik3@gmail.com>
2024-12-07 00:02:37 +0530

    user logout done

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,16 @@ const express = require("express");
 const app = express();
 const connectToDb = require("./db/db");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
+const { cookie } = require("express-validator");
 
 connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
